b7b9361ad5106161daaa7e93fd2cbede6f38d9a6
Johnny Ferreira da Silva <johnnyfs@primeit.com.br>
2020-05-17 12:06:57 -0300

    integracao-marvel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { AppComponentService } from './services/app-component.service';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,30 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ant-man-challenge';
+
+  constructor(private appComponentService: AppComponentService) {
+
+  }
+  ngOnInit(): void {
+    this.getCharacter(1010801);
+  }
+
+  thumbnail;
+
+
+  getCharacters() {
+    this.appComponentService.getAllCharacters()
+      .subscribe(data => {
+        console.log('Data', data.data.results);
+      });
+  }
+
+  getCharacter(id: number) {
+     this.appComponentService.getChacacterById(id)
+      .subscribe(data => {
+        this.thumbnail =  data.data.results[0].thumbnail;
+      })
+  }
 }
---
+++ src/app/services/app-component.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Config } from '../app-config';
+import { Observable } from 'rxjs';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+
+const httpOptions = ({
+  headers: new HttpHeaders({'Content-Type': 'application/json',
+                            'Access-Control-Allow-Origin': '*'})
+});
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppComponentService {
+
+  TS = '1589651945';
+  API_KEY =  'caae684840600b730c185b0835eefa26';
+  HASH = 'b10757f591ca6001131641ab844d5d87';
+  PARAMS = `?ts=${this.TS}&apikey=${this.API_KEY}&hash=${this.HASH}`;
+
+  baseUrl  = this.config.getBaseUrl();
+
+  constructor(private http: HttpClient, private config: Config) { }
+
+  getAllCharacters(): Observable<any> {
+    return this.http.get<any[]>(`${this.baseUrl}/characters${this.PARAMS}&limit=7`, httpOptions );
+  }
+
+  getChacacterById(id: number) : Observable<any> {
+    return this.http.get<any[]>(`${this.baseUrl}/characters/${id}${this.PARAMS}`, httpOptions ); 
+   }
+
+}
